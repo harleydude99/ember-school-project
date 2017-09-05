bfd90568ab32ebafedb18ffb190d34b618bc25a5
Harley Dude <harleydude99@gmail.com>
2017-09-05 14:13:24 -0700

    routes - cleanup and styling, food descr

--- app/models/food.js
+++ app/models/food.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  isAvailable: DS.attr('boolean')
+  isAvailable: DS.attr('boolean'),
+  description: DS.attr('string')
 });
